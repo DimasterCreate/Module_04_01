e850e2e1f9163817f0365cf2a2c6814fcfbb227d
DimasterCreate <d.novs.1977@gmail.com>
2025-01-28 15:14:36 +0300

    Delete true_math.py

--- true_math.py
+++
@@ -1,8 +0,0 @@
-def divide(first, second):
-    if second == 0:
-        print('Полученое частное: ', float('inf'))
-    else:
-        second = int(second)
-        division = round((first / second), 2)
-        print('Полученое частное: ', division)
-
